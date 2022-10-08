72d73328c72a476274be6734da3f29b6ff8d2a1f
M. Rashid <51082957+rashiddaha@users.noreply.github.com>
2022-10-08 23:31:25 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 #STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static/"
+STATIC_ROOT = BASE_DIR / "static"
 #STATIC_ROOT = '/opt/appname/static/'
 
 #STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
